6260d6455149d2361dbf1d6606f5f31f1057e2f1
agj <ale@agj.cl>
2025-11-01 10:54:18 -0300

    Move functions to Num module

--- source/elm/Num.elm
+++ source/elm/Num.elm
@@ -1,4 +1,4 @@
-module Num exposing (Num(..), allBackward, allForward)
+module Num exposing (Num(..), allBackward, allForward, next, toInt)
 
 
 type Num
@@ -45,3 +45,110 @@ allBackward =
     , Num3
     , Num2
     ]
+
+
+next : Bool -> Num -> Maybe Num
+next reverse num =
+    case ( reverse, num ) of
+        ( False, Num2 ) ->
+            Just Num3
+
+        ( False, Num3 ) ->
+            Just Num4
+
+        ( False, Num4 ) ->
+            Just Num5
+
+        ( False, Num5 ) ->
+            Just Num6
+
+        ( False, Num6 ) ->
+            Just Num7
+
+        ( False, Num7 ) ->
+            Just Num8
+
+        ( False, Num8 ) ->
+            Just Num9
+
+        ( False, Num9 ) ->
+            Just Num10
+
+        ( False, Num10 ) ->
+            Just Num11
+
+        ( False, Num11 ) ->
+            Just Num12
+
+        ( False, Num12 ) ->
+            Nothing
+
+        ( True, Num2 ) ->
+            Nothing
+
+        ( True, Num3 ) ->
+            Just Num2
+
+        ( True, Num4 ) ->
+            Just Num3
+
+        ( True, Num5 ) ->
+            Just Num4
+
+        ( True, Num6 ) ->
+            Just Num5
+
+        ( True, Num7 ) ->
+            Just Num6
+
+        ( True, Num8 ) ->
+            Just Num7
+
+        ( True, Num9 ) ->
+            Just Num8
+
+        ( True, Num10 ) ->
+            Just Num9
+
+        ( True, Num11 ) ->
+            Just Num10
+
+        ( True, Num12 ) ->
+            Just Num11
+
+
+toInt : Num -> Int
+toInt num =
+    case num of
+        Num2 ->
+            2
+
+        Num3 ->
+            3
+
+        Num4 ->
+            4
+
+        Num5 ->
+            5
+
+        Num6 ->
+            6
+
+        Num7 ->
+            7
+
+        Num8 ->
+            8
+
+        Num9 ->
+            9
+
+        Num10 ->
+            10
+
+        Num11 ->
+            11
+
+        Num12 ->
+            12
